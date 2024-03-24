0f7aa57c0a93f7fc1bd9ac6379967dc818a7918f
allenlin <ht016746@gmail.com>
2024-03-24 10:06:06 +0700

    feat: undo and redo with key handler

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,14 @@ import {
   renderCanvas,
 } from '@/lib/canvas';
 import { ActiveElement } from '@/types/type';
-import { useMutation, useStorage } from '@/liveblocks.config';
+import { useMutation, useRedo, useStorage, useUndo } from '@/liveblocks.config';
 import { defaultNavElement } from '@/constants';
-import { handleDelete } from '@/lib/key-events';
+import { handleDelete, handleKeyDown } from '@/lib/key-events';
 
 export default function Page() {
+  const undo = useUndo();
+  const redo = useRedo();
+
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const fabricRef = useRef<fabric.Canvas | null>(null);
   const isDrawing = useRef<boolean>(false);
@@ -127,6 +130,17 @@ export default function Page() {
 
     window.addEventListener('resize', () => {
       handleResize({ fabricRef });
+    });
+
+    window.addEventListener('keydown', (e) => {
+      handleKeyDown({
+        e,
+        canvas: fabricRef.current,
+        undo,
+        redo,
+        syncShapeInStorage,
+        deleteShapeFromStorage,
+      });
     });
 
     return () => {
