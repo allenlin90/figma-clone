966b0854a063f492875d63d292f4b25ecdf1eb1b
allenlin <ht016746@gmail.com>
2024-03-24 09:51:52 +0700

    Squashed commit of the following:
    commit 524733fde87d36d3c0bd94e333130b0511bf9a71
    Author: allenlin <ht016746@gmail.com>
    Date:   Sun Mar 24 09:51:26 2024 +0700
    
        docs: liveblocks doc and data storage
    
    commit 6608b098deed07f839af01de1e32665fc38528e5
    Author: allenlin <ht016746@gmail.com>
    Date:   Sun Mar 24 09:51:10 2024 +0700
    
        feat: delete a singel object from canvas and storage
    
    commit 000e7a63f5ac44647c289e205c50738f2ce7d00e
    Author: allenlin <ht016746@gmail.com>
    Date:   Sun Mar 24 09:40:16 2024 +0700
    
        chore: reset and delete all objects on canvas
    
    commit 012bf83ff0e4e6804673b63221fe92a08290883e
    Author: allenlin <ht016746@gmail.com>
    Date:   Sun Mar 24 09:33:47 2024 +0700
    
        chore: sync object move on canvas
    
    commit 41a3e5c94a15cd1a2bc8ba82a34cb10ddc1bf95f
    Author: allenlin <ht016746@gmail.com>
    Date:   Sun Mar 24 09:21:20 2024 +0700
    
        chore: rerender to sync between users
    
    commit 79c7b248a774ad87d741625d59febbc057442bf8
    Author: allenlin <ht016746@gmail.com>
    Date:   Sun Mar 24 09:15:01 2024 +0700
    
        chore: handle mouse up event
    
    commit da8937dc32f09ff2f38ef4bee92e77454e9de495
    Author: allenlin <ht016746@gmail.com>
    Date:   Sun Mar 24 09:14:47 2024 +0700
    
        chore: clear typo
    
    commit f9ebe939cfb493e5415387308c0c040694b0c91f
    Author: allenlin <ht016746@gmail.com>
    Date:   Sun Mar 24 09:09:14 2024 +0700
    
        chore: init liveblocks data storage

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,17 @@ import LeftSidebar from '@/components/LeftSidebar';
 import RightSidebar from '@/components/RightSidebar';
 import {
   handleCanvasMouseDown,
+  handleCanvasMouseMove,
+  handleCanvasMouseUp,
+  handleCanvasObjectModified,
   handleResize,
   initializeFabric,
+  renderCanvas,
 } from '@/lib/canvas';
 import { ActiveElement } from '@/types/type';
+import { useMutation, useStorage } from '@/liveblocks.config';
+import { defaultNavElement } from '@/constants';
+import { handleDelete } from '@/lib/key-events';
 
 export default function Page() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -19,6 +26,22 @@ export default function Page() {
   const isDrawing = useRef<boolean>(false);
   const shapeRef = useRef<fabric.Object | null>(null);
   const selectedShapeRef = useRef<string | null>('rectangle');
+  const activeObjectRef = useRef<fabric.Object | null>(null);
+
+  const canvasObjects = useStorage((root) => root.canvasObjects);
+
+  const syncShapeInStorage = useMutation(({ storage }, object) => {
+    if (!object) return;
+
+    const { objectId } = object;
+
+    const shapeData = object.toJSON();
+    shapeData.objectId = objectId;
+
+    const canvasObjects = storage.get('canvasObjects');
+
+    canvasObjects.set(objectId, shapeData);
+  }, []);
 
   const [activeElement, setActiveElement] = useState<ActiveElement>({
     name: '',
@@ -26,8 +49,38 @@ export default function Page() {
     icon: '',
   });
 
+  const deleteAllShapes = useMutation(({ storage }) => {
+    const canvasObjects = storage.get('canvasObjects');
+
+    if (!canvasObjects || canvasObjects.size === 0) return true;
+
+    for (const [key, _value] of canvasObjects.entries()) {
+      canvasObjects.delete(key);
+    }
+
+    return canvasObjects.size === 0;
+  }, []);
+
+  const deleteShapeFromStorage = useMutation(({ storage }, objectId) => {
+    const canvasObjects = storage.get('canvasObjects');
+
+    canvasObjects.delete(objectId);
+  }, []);
+
   const handleActiveElement = (elem: ActiveElement) => {
     setActiveElement(elem);
+
+    switch (elem?.value) {
+      case 'reset':
+        deleteAllShapes();
+        fabricRef.current?.clear();
+        setActiveElement(defaultNavElement);
+        break;
+      case 'delete':
+        handleDelete(fabricRef.current as any, deleteShapeFromStorage);
+        setActiveElement(defaultNavElement);
+        break;
+    }
 
     selectedShapeRef.current = elem?.value as string;
   };
@@ -45,12 +98,46 @@ export default function Page() {
       });
     });
 
+    canvas.on('mouse:move', (options) => {
+      handleCanvasMouseMove({
+        options,
+        canvas,
+        isDrawing,
+        shapeRef,
+        selectedShapeRef,
+        syncShapeInStorage,
+      });
+    });
+
+    canvas.on('mouse:up', (_options) => {
+      handleCanvasMouseUp({
+        canvas,
+        isDrawing,
+        shapeRef,
+        selectedShapeRef,
+        syncShapeInStorage,
+        setActiveElement,
+        activeObjectRef,
+      });
+    });
+
+    canvas.on('object:modified', (options) => {
+      handleCanvasObjectModified({ options, syncShapeInStorage });
+    });
+
     window.addEventListener('resize', () => {
       handleResize({ fabricRef });
     });
 
-    return () => {};
+    return () => {
+      // clear cached objects when re-render
+      canvas.dispose();
+    };
   }, []);
+
+  useEffect(() => {
+    renderCanvas({ fabricRef, canvasObjects, activeObjectRef });
+  }, [canvasObjects]);
 
   return (
     <main className='h-screen overflow-hidden'>
